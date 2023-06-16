164a932d9f0c177f80f653d821851c6b9e6cf57e
Qaiser Abbas <mqaiser617@gmail.com>
2023-06-16 20:56:39 +0500

    Create text_classification.py

---
+++ gpt-nlp/text_classification.py
@@ -0,0 +1,3 @@
+prompt = """You are a large language model who can do variety of NLP tasks. I will provide you a sentence and you should return the class of that sentence whether it is medical or non-medical, as a json object. Do not produce any additional text and if you do not know the sentiment, just return a json object with unknown sentiment type. Your response should be in following format. {"sentiment": sentiment_class}
+
+sentence: He is working in dermatology for last five months"""
