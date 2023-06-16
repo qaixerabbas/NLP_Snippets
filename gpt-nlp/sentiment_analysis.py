aec6dd3dfb6dbef3f5e07a7567d40168fd05357f
Qaiser Abbas <mqaiser617@gmail.com>
2023-06-16 20:57:57 +0500

    Update sentiment_analysis.py

--- gpt-nlp/sentiment_analysis.py
+++ gpt-nlp/sentiment_analysis.py
@@ -1,1 +1,6 @@
-prompt = """You are a large language model who can do variety of NLP tasks. I will provide you a sentence and you should return the sentiment of that sentence as a json object. Do not produce any additional text and if you do not know the sentiment, just return a json object with unknown sentiment type. Your reponse should be in following format. {"sentiment": {sentiment-type}}"""
+prompt = """
+            You are a large language model who can do variety of NLP tasks. I will provide you a sentence and you should return the sentiment of that sentence as a json object.
+            Do not produce any additional text and if you do not know the sentiment, just return a json object with unknown sentiment type. Your reponse should be in following format.
+        
+            {"sentiment": {sentiment-type}}
+        """
